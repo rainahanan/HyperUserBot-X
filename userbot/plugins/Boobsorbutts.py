c7e6584a2807b00dab2f409cc75ac7ec133675bc
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-14 20:26:22 +0530

    Update Boobsorbutts.py

--- userbot/plugins/Boobsorbutts.py
+++ userbot/plugins/Boobsorbutts.py
@@ -11,9 +11,9 @@ from . import TMP_DOWNLOAD_DIRECTORY
 @borg.on(admin_cmd(pattern=r"boobs"))
 async def boobs(event):
     pic_loc = os.path.join(TMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
-    a = await event.edit("`Finding some big bobs 🧐...`")
+    a = await event.edit("`SAALO THARKI NALLE HII MAROGE TUM LOG😂🙏...`")
     await asyncio.sleep(0.5)
-    await a.edit("`Sending some big bobs 🌚...`")
+    await a.edit("`RUKO ABHI AAYEGA BxxBS...`")
     nsfw = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
     urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
     await event.client.send_file(event.chat_id, pic_loc, force_document=False)
@@ -25,9 +25,9 @@ async def boobs(event):
 @borg.on(admin_cmd(pattern=r"butts"))
 async def butts(event):
     pic_loc = os.path.join(TMP_DOWNLOAD_DIRECTORY, "butts.jpg")
-    a = await event.edit("`Finding some beautiful butts 🧐...`")
+    a = await event.edit("`SAALO THARKI NALLE HII MAROGE TUM LOG🙏😂...`")
     await asyncio.sleep(0.5)
-    await a.edit("`Sending some beautiful butts 🌚...`")
+    await a.edit("`RUKO JARA ITNI BHI KYA JALDI HAI BUxTS DEKHNEKI...`")
     nsfw = requests.get("http://api.obutts.ru/noise/1").json()[0]["preview"]
     urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), pic_loc)
     await event.client.send_file(event.chat_id, pic_loc, force_document=False)
