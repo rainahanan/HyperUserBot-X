0425c966bbc981b3501afcd34f8da40d7fb37fea
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-16 03:18:25 +0530

    Create impressed.py

---
+++ userbot/plugins/impressed.py
@@ -0,0 +1,9 @@
+import random, re
+from ..utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="impressgf ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit(" `meri gf bnjao tumhari har icha poori karuga .... Jo tum kahogi wo kruga plzz.. ban jao ..... I love you baby ....... Tumhare saare nakhre seh luga babes ..... I love you.... Maan jao 🥺🥺!\n")
