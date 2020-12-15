4e109220eda58c4e9e6b354aabbe3e8f813f7414
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-15 21:32:23 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -219,7 +219,9 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             else:
                 reply_pop_up_alert = help_string
             reply_pop_up_alert += (
-                "Use .unload {} to remove this plugin © HyperUserBot-X".format(plugin_name)
+                "Use .unload {} to remove this plugin © HyperUserBot-X".format(
+                    plugin_name
+                )
             )
             try:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
