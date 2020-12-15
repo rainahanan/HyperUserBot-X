18f73423df11a9abcdf219c6fef96ed6d41b0910
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-16 03:24:12 +0530

    Create tapatap.py

---
+++ userbot/plugins/tapatap.py
@@ -0,0 +1,20 @@
+import random, re
+from ..utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="tapatap ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+
+        await event.edit("Nipple 😋 se Tapak Raha Hai Pasina😥")
+        await asyncio.sleep(2)
+        await event.edit("Nipple 🤭 se Tapak Rha h pasina🤤")
+        await asyncio.sleep(2)
+        await event.edit("Bhegi💦 hui gand, Aur lathpath Seena🤭")
+        await asyncio.sleep(2)
+        await event.edit("Abb Tumhi btao Bhai🙄")
+        await asyncio.sleep(2)
+        await event.edit("Itni garmi me😰 \n Koi Kaise Thoke Hasina😵")
+        await asyncio.sleep(3.5)
+        await event.edit("Nipple Se Tapak Raha Hai Pasina💦 \nNipple Se Tapak Raha Hai Pasina💦 \nBhigi Hui Gaand🤭 Aur Lathpath Seena😐 \nAab Tumhi Batao bhai🙄 \nItni Garmi Mein😰 Koi Kaise Thoke😵 Hasina😋")
