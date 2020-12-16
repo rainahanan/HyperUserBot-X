46923e4aea699e0fb7c96a9f4e05b07cab6e79de
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-16 05:47:27 +0000

    pylint: auto fixes

--- userbot/plugins/dp.py
+++ userbot/plugins/dp.py
@@ -1,12 +1,8 @@
-from telethon import events
 import asyncio
-import os
-import sys
-import random
-from userbot import ALIVE_NAME, CMD_HELP
-from ..utils import admin_cmd, edit_or_reply
 
+from userbot import ALIVE_NAME
 
+from ..utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "HyperUserBot-X"
 
@@ -15,20 +11,28 @@ aryan = bot.uid
 
 @borg.on(admin_cmd(pattern="dp ?(.*)"))
 async def _(event):
-        if event.fwd_from:
-            return
-        await event.edit("😎Jaha matter bada hota hai😎")
-        await asyncio.sleep(2)
-        await event.edit("{}😈wahi khada hota hai 💪".format(DEFAULTUSER,aryan))
-        await asyncio.sleep(2)
-        await event.edit("😡Tera baap hu madharchod😒")
-        await asyncio.sleep(2)
-        await event.edit("😏Naam yad rakhna😏")
-        await asyncio.sleep(2)
-        await event.edit("😎Tera baap {}🤓".format(DEFAULTUSER,aryan))
-        await asyncio.sleep(2)
-        await event.edit("🔥{}🔥".format(DEFAULTUSER,a))
-        await asyncio.sleep(2)
-        await event.edit("😎Jaha matter bada hota hai😎. {} wahi khada hota hai😏".format(DEFAULTUSER,aryan))
-        await asyncio.sleep(2)
-        await event.edit("Tera baap hu madharchod😈. Naam yad rakhna😡. Tera baap {}. 🔥🔥🔥🔥🔥".format(DEFAULTUSER,aryan))
+    if event.fwd_from:
+        return
+    await event.edit("😎Jaha matter bada hota hai😎")
+    await asyncio.sleep(2)
+    await event.edit("{}😈wahi khada hota hai 💪".format(DEFAULTUSER, aryan))
+    await asyncio.sleep(2)
+    await event.edit("😡Tera baap hu madharchod😒")
+    await asyncio.sleep(2)
+    await event.edit("😏Naam yad rakhna😏")
+    await asyncio.sleep(2)
+    await event.edit("😎Tera baap {}🤓".format(DEFAULTUSER, aryan))
+    await asyncio.sleep(2)
+    await event.edit("🔥{}🔥".format(DEFAULTUSER, a))
+    await asyncio.sleep(2)
+    await event.edit(
+        "😎Jaha matter bada hota hai😎. {} wahi khada hota hai😏".format(
+            DEFAULTUSER, aryan
+        )
+    )
+    await asyncio.sleep(2)
+    await event.edit(
+        "Tera baap hu madharchod😈. Naam yad rakhna😡. Tera baap {}. 🔥🔥🔥🔥🔥".format(
+            DEFAULTUSER, aryan
+        )
+    )
