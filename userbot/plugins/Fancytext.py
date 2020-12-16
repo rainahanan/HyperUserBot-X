ca1e1a614f75e568c8d1cda1e0f2151f1218ba94
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-16 05:53:15 +0000

    pylint: auto fixes

--- userbot/plugins/Fancytext.py
+++ userbot/plugins/Fancytext.py
@@ -1,54 +1,68 @@
-import random, re
-from ..utils import admin_cmd
 import asyncio
-from telethon import events
+
+from ..utils import admin_cmd
+
 
 @borg.on(admin_cmd(pattern="mst ?(.*)"))
 async def _(event):
-        await event.edit("mst hu bbro ")
-        await asyncio.sleep(1)
-        await event.edit("╔═╦═╗╔══╗╔══╗\n║║║║║║══╣╚╗╔╝\n║║║║║╠══║─║║─\n╚╩═╩╝╚══╝─╚╝─")
+    await event.edit("mst hu bbro ")
+    await asyncio.sleep(1)
+    await event.edit("╔═╦═╗╔══╗╔══╗\n║║║║║║══╣╚╗╔╝\n║║║║║╠══║─║║─\n╚╩═╩╝╚══╝─╚╝─")
+
 
 @borg.on(admin_cmd(pattern="gm ?(.*)"))
 async def _(event):
-        await event.edit("good morning ")
-        await asyncio.sleep(1)
-        await event.edit("╔══╗╔═╦═╗\n║╔═╣║║║║║\n║╚╗║║║║║║\n╚══╝╚╩═╩╝")
+    await event.edit("good morning ")
+    await asyncio.sleep(1)
+    await event.edit("╔══╗╔═╦═╗\n║╔═╣║║║║║\n║╚╗║║║║║║\n╚══╝╚╩═╩╝")
+
 
 @borg.on(admin_cmd(pattern="good ?(.*)"))
 async def _(event):
-        await event.edit("╔══╗╔═╗╔═╗╔══╗\n║╔═╣║║║║║║╚╗╗║\n║╚╗║║║║║║║╔╩╝║\n╚══╝╚═╝╚═╝╚══╝")
+    await event.edit("╔══╗╔═╗╔═╗╔══╗\n║╔═╣║║║║║║╚╗╗║\n║╚╗║║║║║║║╔╩╝║\n╚══╝╚═╝╚═╝╚══╝")
+
 
 @borg.on(admin_cmd(pattern="hhlo ?(.*)"))
 async def _(event):
-        await event.edit("hello,how r u")
-        await asyncio.sleep(1)
-        await event.edit("╔╗╔╗╔╗─╔═╗\n║╚╝║║║─║║║\n║╔╗║║╚╗║║║\n╚╝╚╝╚═╝╚═╝")
+    await event.edit("hello,how r u")
+    await asyncio.sleep(1)
+    await event.edit("╔╗╔╗╔╗─╔═╗\n║╚╝║║║─║║║\n║╔╗║║╚╗║║║\n╚╝╚╝╚═╝╚═╝")
+
 
 @borg.on(admin_cmd(pattern="sry ?(.*)"))
 async def _(event):
-        await event.edit("i'm sorry")
-        await asyncio.sleep(1)
-        await event.edit("last time forgive me")
-        await asyncio.sleep(1)
-        await event.edit("╔══╗╔═╗╔═╗╔═╗╔═╦╗\n║══╣║║║║╬║║╬║╚╗║║\n╠══║║║║║╗╣║╗╣╔╩╗║\n╚══╝╚═╝╚╩╝╚╩╝╚══╝")
+    await event.edit("i'm sorry")
+    await asyncio.sleep(1)
+    await event.edit("last time forgive me")
+    await asyncio.sleep(1)
+    await event.edit(
+        "╔══╗╔═╗╔═╗╔═╗╔═╦╗\n║══╣║║║║╬║║╬║╚╗║║\n╠══║║║║║╗╣║╗╣╔╩╗║\n╚══╝╚═╝╚╩╝╚╩╝╚══╝"
+    )
+
 
 @borg.on(admin_cmd(pattern="thnq ?(.*)"))
 async def _(event):
-        await event.edit("thanks for your help")
-        await asyncio.sleep(1)
-        await event.edit("╔══╗╔╗╔╗╔══╗╔═╦╗╔╦╗╔══╗\n╚╗╔╝║╚╝║║╔╗║║║║║║╔╝║══╣\n─║║─║╔╗║║╠╣║║║║║║╚╗╠══║\n─╚╝─╚╝╚╝╚╝╚╝╚╩═╝╚╩╝╚══╝")
+    await event.edit("thanks for your help")
+    await asyncio.sleep(1)
+    await event.edit(
+        "╔══╗╔╗╔╗╔══╗╔═╦╗╔╦╗╔══╗\n╚╗╔╝║╚╝║║╔╗║║║║║║╔╝║══╣\n─║║─║╔╗║║╠╣║║║║║║╚╗╠══║\n─╚╝─╚╝╚╝╚╝╚╝╚╩═╝╚╩╝╚══╝"
+    )
+
 
 @borg.on(admin_cmd(pattern="ok ?(.*)"))
 async def _(event):
-        await event.edit("▒▐█▀▀█▌▒▐█▒▐▀\n▒▐█▄▒█▌▒▐██▌░\n▒▐██▄█▌▒▐█▒▐▄")
+    await event.edit("▒▐█▀▀█▌▒▐█▒▐▀\n▒▐█▄▒█▌▒▐██▌░\n▒▐██▄█▌▒▐█▒▐▄")
+
 
 @borg.on(admin_cmd(pattern="smile ?(.*)"))
 async def _(event):
-        await event.edit("how sad ")
-        await asyncio.sleep(1)
-        await event.edit("╔══╗╔═╦═╗╔══╗╔╗─╔═╗\n║══╣║║║║║╚║║╝║║─║╦╝\n╠══║║║║║║╔║║╗║╚╗║╩╗\n╚══╝╚╩═╩╝╚══╝╚═╝╚═╝")
+    await event.edit("how sad ")
+    await asyncio.sleep(1)
+    await event.edit(
+        "╔══╗╔═╦═╗╔══╗╔╗─╔═╗\n║══╣║║║║║╚║║╝║║─║╦╝\n╠══║║║║║║╔║║╗║╚╗║╩╗\n╚══╝╚╩═╩╝╚══╝╚═╝╚═╝"
+    )
+
 
 @borg.on(admin_cmd(pattern="lal ?(.*)"))
 async def _(event):
-        await event.edit("╔╗─╔═╗╔╗─\n║╚╗║╬║║╚╗\n╚═╝╚═╝╚═╝")
+    await event.edit("╔╗─╔═╗╔╗─\n║╚╗║╬║║╚╗\n╚═╝╚═╝╚═╝")
