cbbe70b00bca1729059601bb7a7d2408298027b6
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-16 05:34:20 +0000

    pylint: auto fixes

--- userbot/plugins/girls.py
+++ userbot/plugins/girls.py
@@ -4,7 +4,6 @@ from ..utils import admin_cmd
 @borg.on(admin_cmd(pattern="dad ?(.*)"))
 async def _(event):
 
-
-        await event.edit(
-            " `hey girls my father didn't fuck ur mother then why u call me brother ? \n"
-        )
+    await event.edit(
+        " `hey girls my father didn't fuck ur mother then why u call me brother ? \n"
+    )
