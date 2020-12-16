fe1bd3d2b792e45f4903fbaa45e4ec29118bd236
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-16 11:16:35 +0530

    Create dp.py

---
+++ userbot/plugins/dp.py
@@ -0,0 +1,34 @@
+from telethon import events
+import asyncio
+import os
+import sys
+import random
+from userbot import ALIVE_NAME, CMD_HELP
+from ..utils import admin_cmd, edit_or_reply
+
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "HyperUserBot-X"
+
+aryan = bot.uid
+
+
+@borg.on(admin_cmd(pattern="dp ?(.*)"))
+async def _(event):
+        if event.fwd_from:
+            return
+        await event.edit("😎Jaha matter bada hota hai😎")
+        await asyncio.sleep(2)
+        await event.edit("{}😈wahi khada hota hai 💪".format(DEFAULTUSER,aryan))
+        await asyncio.sleep(2)
+        await event.edit("😡Tera baap hu madharchod😒")
+        await asyncio.sleep(2)
+        await event.edit("😏Naam yad rakhna😏")
+        await asyncio.sleep(2)
+        await event.edit("😎Tera baap {}🤓".format(DEFAULTUSER,aryan))
+        await asyncio.sleep(2)
+        await event.edit("🔥{}🔥".format(DEFAULTUSER,a))
+        await asyncio.sleep(2)
+        await event.edit("😎Jaha matter bada hota hai😎. {} wahi khada hota hai😏".format(DEFAULTUSER,aryan))
+        await asyncio.sleep(2)
+        await event.edit("Tera baap hu madharchod😈. Naam yad rakhna😡. Tera baap {}. 🔥🔥🔥🔥🔥".format(DEFAULTUSER,aryan))
