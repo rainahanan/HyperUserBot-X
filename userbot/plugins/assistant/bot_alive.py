3d45ff187ef8919066bacb798a8d66f8b5b04f6b
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-17 18:06:21 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -12,8 +12,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from ..utils import catversion
-from . import ALIVE_NAME
+from . import ALIVE_NAME, catversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/22535f8051a58af113586.jpg"
