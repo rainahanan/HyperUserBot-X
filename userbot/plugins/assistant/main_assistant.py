f6a9ea301fdea3c37f0a95509ea03edc919bfa44
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-18 08:51:16 +0000

    pylint: auto fixes

--- userbot/plugins/assistant/main_assistant.py
+++ userbot/plugins/assistant/main_assistant.py
@@ -19,6 +19,7 @@ import re
 from telethon import Button, custom, events
 from telethon.tl.functions.users import GetFullUserRequest
 
+from userbot import bot
 from userbot.Config import Config
 from userbot.plugins.sql_helper.blacklist_assistant import (
     add_nibba_in_db,
@@ -31,8 +32,6 @@ from userbot.plugins.sql_helper.idadder_sql import (
     already_added,
     get_all_users,
 )
-
-from userbot import bot
 
 
 @admin_cmd("start", is_args=False)
