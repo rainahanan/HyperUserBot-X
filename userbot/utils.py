861410c239811603b70bd4513185187393a021a4
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-18 15:14:52 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -17,6 +17,7 @@ from telethon import events
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
+from .function import userbotfunction as topfunc
 from . import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from .helpers.exceptions import CancelProcess
 
