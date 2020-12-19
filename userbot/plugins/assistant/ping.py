730468fd989e8614e99d54b57f132b947731461d
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-19 16:38:56 +0530

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -52,7 +52,6 @@ async def _(event):
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    uptime = get_readable_time((time.time() - catversion))
     await tgbot.send_message(
         event.chat_id,
         f"**█▀█ █▀█ █▄░█ █▀▀ █ \n█▀▀ █▄█ █░▀█ █▄█ ▄**\n ➲ `{ms}` \n ➲ `{uptime}`",
