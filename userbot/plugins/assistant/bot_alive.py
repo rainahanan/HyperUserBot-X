387d797703d252ea4794cfd6efb5fa37427b8ac0
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-28 23:30:05 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -2,23 +2,22 @@ from telethon import events
 
 from userbot.plugins import catversion
 
-from . import ALIVE_NAME, bot
+from userbot import bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "HyperUser"
 
 
 PM_IMG = "https://telegra.ph/file/c92b6368929e1e44112d5.jpg"
-pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.7.4` \n"
-pm_caption += "➥ **Database Status:**  `Functional`\n"
-pm_caption += "➥ **Current Branch** : `main`\n"
-pm_caption += f"➥ **Version** : `{catversion}`\n"
-pm_caption += f"➥ **My Boss** : {DEFAULTUSER}\n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/ahirearyan2/HyperUserBot-X/blob/main/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [TEAM HyperUserBot-X](GitHub.com/ahirearyan2/HyperUserBot-X)\n"
+pm_caption = "✥ **ASSISTANT IS:** `ONLINE`\n\n"
+pm_caption += "✥ **SYSTEMS STATS**\n"
+pm_caption += "✥ **Telethon Version:** `1.15.0` \n"
+pm_caption += "✥ **Python:** `3.7.4` \n"
+pm_caption += "✥ **Database Status:**  `Functional`\n"
+pm_caption += "✥ **Current Branch** : `main`\n"
+pm_caption += f"✥ **Version** : `{catversion}`\n"
+pm_caption += "✥ **Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "✥ **License** : [GNU General Public License v3.0](github.com/ahirearyan2/HyperUserBot-X/blob/main/LICENSE)\n"
+pm_caption += "✥ **Copyright** : By [TEAM HyperUserBot-X](GitHub.com/ahirearyan2/HyperUserBot-X)\n"
 pm_caption += "[Assistant By HyperUserBot-X ]"
 
 # only Owner Can Use it
